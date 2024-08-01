44a3ee7c5e33217f863249b46abfcb0ab87b9f6c
DigitalMoteur <digitalmoteur@outlook.com>
2024-08-01 00:21:10 -0700

    Initial commit, empty project

---
+++ HWSChallenge1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HWSChallenge1
+//
+//  Created by Christopher Wade on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HWSChallenge1/HWSChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  HWSChallenge1App.swift
+//  HWSChallenge1
+//
+//  Created by Christopher Wade on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HWSChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
